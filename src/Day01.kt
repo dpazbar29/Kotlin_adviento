abe42a15809592a92a333fe415c6995b07b9fc6e
dpazbar <dpazbar@g.educaand.es>
2023-12-18 10:00:01 +0100

    18/12/2023 10:00

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): List<String> {
-        return input
+    fun part1(input: List<String>): Int {
+        var cont = 0
+        val salida = mutableListOf<String>()
+        while (cont < 999){
+            for (caracter in input[cont]){
+                val numeros = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+                if (numeros.toString().contains(caracter)){
+                    salida.add(caracter.toString())
+                }
+            }
+            println(salida)
+            cont++
+        }
+        return input.size
     }
 
     fun part2(input: List<String>): Int {
